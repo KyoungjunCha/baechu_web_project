760ea99f18fe74ba793ea55ae986cddca5364077
tobetop123 <tobetop123@gmail.com>
2024-01-11 21:20:20 +0900

    MyPostList

--- baechu-front/src/App.js
+++ baechu-front/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import BoardList from "./components/BoardList";
 import "./index.css";
+import MyPostList from "./pages/MyPostList";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       <div className="centerText">
         <p>invisible something</p>
       </div>
-      <BoardList />
+      <MyPostList/>
+      {/* <BoardList /> */}
       <Footer />
     </div>
   );
---
+++ baechu-front/src/pages/PostDummy.js
@@ -0,0 +1,91 @@
+const dummyData = [
+    { 
+        id: 1, 
+        hasImage: true, 
+        title: '수원 맛집 추천!', 
+        author: '곽두팔', 
+        date: '2024-01-03' 
+    },
+    { 
+        id: 2, 
+        hasImage: false, 
+        title: '망포 맛집 추천!', 
+        author: '곽기두', 
+        date: '2024-01-03' 
+    },
+
+    { 
+        id: 3, 
+        hasImage: true, 
+        title: '수원 술집 추천!', 
+        author: '김철수', 
+        date: '2024-01-03' 
+      },
+    { 
+        id: 4, 
+        hasImage: false, 
+        title: '먹고살기 힘들다!', 
+        author: '김꽃분', 
+        date: '2024-01-03'  
+    },
+    { 
+        id: 5, 
+        hasImage: true, 
+        title: '수원 맛집 추천!', 
+        author: '곽두팔', 
+        date: '2024-01-03' 
+    },
+    { 
+        id: 6, 
+        hasImage: false, 
+        title: '망포 맛집 추천!', 
+        author: '곽기두', 
+        date: '2024-01-03' 
+    },
+
+    { 
+        id: 7, 
+        hasImage: true, 
+        title: '수원 술집 추천!', 
+        author: '김철수', 
+        date: '2024-01-03' 
+      },
+    { 
+        id: 8, 
+        hasImage: false, 
+        title: '먹고살기 힘들다!', 
+        author: '김꽃분', 
+        date: '2024-01-03'  
+    },
+    { 
+        id: 9, 
+        hasImage: true, 
+        title: '수원 맛집 추천!', 
+        author: '곽두팔', 
+        date: '2024-01-03' 
+    },
+    { 
+        id: 10, 
+        hasImage: false, 
+        title: '망포 맛집 추천!', 
+        author: '곽기두', 
+        date: '2024-01-03' 
+    },
+
+    { 
+        id: 11, 
+        hasImage: true, 
+        title: '수원 술집 추천!', 
+        author: '김철수', 
+        date: '2024-01-03' 
+      },
+    { 
+        id: 12, 
+        hasImage: false, 
+        title: '먹고살기 힘들다!', 
+        author: '김꽃분', 
+        date: '2024-01-03'  
+    },
+  ];
+
+  export default dummyData;
