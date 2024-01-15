da6d20eba8ee519f296dcf881f3e884a916727cf
tobetop123 <tobetop123@gmail.com>
2024-01-15 21:50:42 +0900

    PostWrite 페이지및 DropdownMenu 기능 추가

---
+++ baechu-front/src/dummy/Category.js
@@ -0,0 +1,16 @@
+const categoryOptions = [
+    {
+      id: 1,
+      category: '음식',
+    },
+    {
+      id: 2,
+      category: '전자기기',
+    },
+    {
+      id: 3,
+      category: '화장품',
+    },
+  ];
+
+  export default categoryOptions;
