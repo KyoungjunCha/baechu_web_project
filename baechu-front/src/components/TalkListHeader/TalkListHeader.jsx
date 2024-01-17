29e4562d56ada19d7978ad8288a1bafda968ee2c
cha <zktmzkxh1234@gmail.com>
2024-01-18 01:52:22 +0900

    미친 header 만들다가 2시까지 왔어

--- baechu-front/src/components/TalkListHeader/TalkListHeader.jsx
+++ baechu-front/src/components/TalkListHeader/TalkListHeader.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import "./TalkListHeader.css"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export default function TalkListHeader() {
-  return (
-    <div className='TalkListHeaderWrap'>
-        <div className='TalkAddress'>
-            경기도 수원시 장안구
+    return (
+        <div className='TalkListHeaderWrap'>
+            <div className='TalkAddress'>
+                경기도 수원시 장안구
+            </div>
+            <div className='TalkSearch'>
+                <input type="text" placeholder="검색어를 입력하세요" />
+                <button type="button">
+                    <FontAwesomeIcon icon={faSearch} aria-label="search icon" />
+                </button>
+            </div>
+            <div className='TalkCategory'>
+                카테고리들
+            </div>
         </div>
-        <div className='TalkSearch'>
-            돋보기
-        </div>
-        <div className='TalkCategory'>
-            카테고리들
-        </div>
-    </div>
-  )
+    )
 }
