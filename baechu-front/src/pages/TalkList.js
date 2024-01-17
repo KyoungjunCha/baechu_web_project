451e76528ac5065de21301a31347e34cdf3c8df7
cha <zktmzkxh1234@gmail.com>
2024-01-17 17:54:44 +0900

    test

--- baechu-front/src/pages/TalkList.js
+++ baechu-front/src/pages/TalkList.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import imageIcon from '../images/imgy.png';
 import noImageIcon from '../images/imgn.png';
 import trashIcon from '../images/trashcan-icon-white.png';
-import '../css/MyCommentList.css';
+// import '../css/MyCommentList.css';
 import dummyData from '../dummy/CommentDummy'
 
 const pageSize = 5; // 한 페이지당 아이템 수를 5로 변경
