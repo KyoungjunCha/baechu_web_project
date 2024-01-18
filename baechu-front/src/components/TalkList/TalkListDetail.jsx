122c67eca640e16c0ae9d92e58354a48996a5836
cha <zktmzkxh1234@gmail.com>
2024-01-18 21:18:39 +0900

    test

--- baechu-front/src/components/TalkList/TalkListDetail.jsx
+++ baechu-front/src/components/TalkList/TalkListDetail.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
-// import 'TalkListDetail.css'
+import React from 'react';
+import './TalkListDetail.css';
 
+const dummyDatas = [
+  {
+    id: 1,
+    category: '음식',
+    image:"../../images/imgg.png",
+    title: '수원 맛집 가성비 판별',
+    personnel: 55,
+    agree:42,
+    disagree: 55
+  },
+  {
+    id: 2,
+    category: '카페',
+    image:"../../images/imgg.png",
+    title: '수원 카페 가성비 판별',
+    personnel: 55,
+    agree:42,
+    disagree: 55
+  },
+  {
+    id: 3,
+    category: '음식',
+    image:"../../images/imgg.png",
+    title: '수원 맛집 가성비 판별',
+    personnel: 55,
+    agree:42,
+    disagree: 55
+  },
+  {
+    id: 4,
+    category: '음식',
+    image:"../../images/imgg.png",
+    title: '수원 맛집 가성비 판별',
+    personnel: 55,
+    agree:42,
+    disagree: 55
+  },
+  {
+    id: 5,
+    category: '음식',
+    image:"../../images/imgg.png",
+    title: '수원 맛집 가성비 판별',
+    personnel: 55,
+    agree:42,
+    disagree: 55
+  },
+  {
+    id: 6,
+    category: '음식',
+    image:"../../images/imgg.png",
+    title: '수원 맛집 가성비 판별',
+    personnel: 55,
+    agree:42,
+    disagree: 55
+  },
+  {
+    id: 7,
+    category: '음식',
+    image:"../../images/imgg.png",
+    title: '수원 맛집 가성비 판별',
+    personnel: 55,
+    agree:42,
+    disagree: 55
+  },
+  {
+    id: 8,
+    category: '음식',
+    image:"../../images/imgg.png",
+    title: '수원 맛집 가성비 판별',
+    personnel: 55,
+    agree:42,
+    disagree: 55
+  },
+];
 
 export default function TalkListDetail() {
   return (
-    <div>TalkListDetail</div>
-  )
+    <div className='TalkListDetailWrap'>
+      <div className='TalkList'>
+        {dummyDatas.map((data) => (
+          <div key={data.id} className='item'>
+            <img src={data.image} alt={data.title} />
+            <div className='title'>{data.title}</div>
+            <div className='category'>{data.category}</div>
+            <div className='personnel'>{data.personnel}</div>
+            <div className='agree'>{data.agree}</div>
+            <div className='disagree'>{data.disagree}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- baechu-front/src/components/TalkListHeader/TalkListHeader.jsx
+++ baechu-front/src/components/TalkListHeader/TalkListHeader.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import "./TalkListHeader.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react';
+import "./TalkListHeader.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const dummyCategorys = [
+    {
+        id: 1,
+        title: '음식'
+    },
+    {
+        id: 2,
+        title: '운동'
+    },
+    {
+        id: 3,
+        title: '카페'
+    },
+    {
+        id: 4,
+        title: '전자기기'
+    },
+    {
+        id: 5,
+        title: '음악'
+    },
+    {
+        id: 6,
+        title: '테스트1'
+    },
+    {
+        id: 7,
+        title: '테스트2'
+    },
+    {
+        id: 8,
+        title: '테스트3'
+    },
+];
 
 export default function TalkListHeader() {
+    // const [visibleCategories, setVisibleCategories] = useState(5);
+
+    // const handleShowMore = () => {
+    //     setVisibleCategories(visibleCategories + 1);
+    // };
+
     return (
         <div className='TalkListHeaderWrap'>
             <div className='TalkAddress'>
@@ -16,8 +57,17 @@ export default function TalkListHeader() {
                 </button>
             </div>
             <div className='TalkCategory'>
-                카테고리들
+                {dummyCategorys.map((category) => (
+                    <div key={category.id} className='categoryItem'>
+                        {category.title}
+                    </div>
+                ))}
             </div>
+            {/* {visibleCategories < dummyCategorys.length && (
+                <button onClick={handleShowMore} className="showMoreButton">
+                    더 보기
+                </button>
+            )} */}
         </div>
-    )
+    );
 }
