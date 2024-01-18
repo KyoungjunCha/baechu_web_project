7ec08d7709a748074401abd55f519f32184083a9
ShadowChopin <mintheking@naver.com>
2024-01-18 11:50:33 +0900

    LoginRecovery

--- baechu-front/src/App.js
+++ baechu-front/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import BoardList from "./components/BoardList";
-import "./index.css";
+import PasswordRecoveryPage from "./pages/PasswordRecoveryPage";
 import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import "./index.css";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <SignUpPage />
-      {/* <BoardList /> */}
-      <Footer />
-    </div>
+    <Router>
+      <div className="page-content">
+        <Header />
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/password-recovery" element={<PasswordRecoveryPage />} />
+          <Route path="/board" element={<BoardList />} />
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
