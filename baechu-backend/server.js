838e668d9b01a40b3a9cf9968bb4edcfc7d903fb
cha <zktmzkxh1234@gmail.com>
2024-01-26 01:06:20 +0900

    axios post test

--- baechu-backend/server.js
+++ baechu-backend/server.js
@@ -35,6 +35,25 @@ app.get('/users', (req, res) => {
   });
 })
 
+// POST 엔드포인트: 사용자 등록을 처리하는 엔드포인트
+app.post('/signup', (req, res) => {
+  const { userNickName, email, password, province, city, birthday } = req.body;
+
+  // 필요에 따라 수신된 데이터를 유효성 검사할 수 있습니다.
+
+  // 사용자 데이터를 데이터베이스에 삽입
+  let query = 'INSERT INTO user (userNickName, email, password, province, city, birthday) VALUES (?, ?, ?, ?, ?, ?)';
+  connection.query(query, [userNickName, email, password, province, city, birthday], (err, results) => {
+    if (err) {
+      console.error('사용자 데이터를 삽입하는 중 에러 발생:', err);
+      return res.status(500).json({ success: false, message: '회원가입에 실패했습니다.' });
+    }
+    
+    console.log('사용자가 성공적으로 등록되었습니다:', results);
+    res.json({ success: true, message: '회원가입이 완료되었습니다.' });
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
